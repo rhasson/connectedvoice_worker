2ac3794b64411676bdb4008f27ac343368382915
rhasson <rhasson@gmail.com>
2015-06-12 10:00:52 -0400

    minor typo

--- libs/helper_functions.js
+++ libs/helper_functions.js
@@ -98,7 +98,7 @@ module.exports = helpers = {
 					}
 					twiml.gather(item.verb_attributes, function(node) {
 						if ('nested' in item && item.nested.length) {
-							if (var j=0; j < item.nested.length; j++) {
+							for (var j=0; j < item.nested.length; j++) {
 								node = create(item.nested[j], node);
 							}
 						}
